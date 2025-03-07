6918f4de05531770f4a1daca667a424b2822c331
Giorgio Rossi <giorgio04.rossi@gmail.com>
2025-03-07 14:50:17 +0100

    "Bug correction"

---
+++ src/main/java/com/rentalcar/backend/dto/LoginResponseDTO.java
@@ -0,0 +1,13 @@
+package com.rentalcar.backend.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+@Builder
+public class LoginResponseDTO {
+    private String token;
+}
---
+++ src/main/java/com/rentalcar/config/JwtUtils.java
@@ -0,0 +1,106 @@
+package com.rentalcar.config;
+
+import com.rentalcar.backend.model.User;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.security.SecureRandom;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+
+@Component
+public class JwtUtils {
+    private static final long EXPIRATION_TIME = 3600000;
+    private final String TOKEN_HEADER = "Authorization";
+    private final String TOKEN_PREFIX = "Bearer ";
+
+    private final Key key;
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public JwtUtils(@Value("${jwt.secret}") String secret) {
+        this.secret = secret;
+        this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        String role = userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .findFirst()
+                .orElse("");
+
+        Date currentDate = new Date();
+        Date expireDate = new Date(currentDate.getTime() + EXPIRATION_TIME);
+
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .claim("role", role)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public Boolean validateToken(String token) {
+        final String username = extractUsername(token);
+        return (!isTokenExpired(token));
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public Date extractExpiration(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getExpiration();
+    }
+
+
+    public String resolveToken(HttpServletRequest request) {
+
+        String bearerToken = request.getHeader(TOKEN_HEADER);
+        if (bearerToken != null && bearerToken.startsWith(TOKEN_PREFIX)) {
+            return bearerToken.substring(TOKEN_PREFIX.length());
+        }
+        return null;
+    }
+
+    public String generateSecretKey() {
+        int length = 32;
+
+        SecureRandom secureRandom = new SecureRandom();
+
+        byte[] keyBytes = new byte[length];
+
+        secureRandom.nextBytes(keyBytes);
+
+        return Base64.getEncoder().encodeToString(keyBytes);
+    }
+
+}
